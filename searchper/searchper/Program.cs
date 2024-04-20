66a47255dfe9799b921440fea397a5430b018f57
katakuriqqe0 <167584432+katakuriqqe0@users.noreply.github.com>
2024-04-20 13:13:39 +0300

    додав можливість пошуку периметра

---
+++ searchper/searchper/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Запитуємо користувача про довжину сторони квадрата
+        Console.Write("Введіть довжину сторони квадрата: ");
+        double a = Convert.ToDouble(Console.ReadLine());
+
+        // Обчислюємо периметр квадрата
+        double perimeter = 4 * a;
+
+        // Виводимо результат
+        Console.WriteLine($"Периметр квадрата зі стороною {a} одиниць дорівнює {perimeter} одиниць.");
+    }
+}
